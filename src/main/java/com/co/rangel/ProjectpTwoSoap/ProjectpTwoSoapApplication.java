406a4380e050deb46fea28eb4c8ecffb58744475
Carlos Rangel <carloseduardo712404@hotmail.com>
2024-08-15 20:49:26 -0500

    Primer commit

---
+++ src/main/java/com/co/rangel/ProjectpTwoSoap/ProjectpTwoSoapApplication.java
@@ -0,0 +1,13 @@
+package com.co.rangel.ProjectpTwoSoap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectpTwoSoapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectpTwoSoapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/co/rangel/ProjectpTwoSoap/ProjectpTwoSoapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.co.rangel.ProjectpTwoSoap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectpTwoSoapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
